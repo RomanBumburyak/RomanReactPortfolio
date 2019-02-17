acdb63d6aba4776140e1bd9c2b4ad73a94d6b875
RomanBumburyak <romanbumburyak@gmail.com>
2019-02-17 03:47:07 -0500

    Website is now deployed and live on www.romanthedev.com. Website re-done and re-deployed within six hours

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 class About extends React.Component {
     render () {
         return (
-            <div style={{color:'orange', height:'100px', backgroundColor:'#333'}}> Hey About </div>
+            <div style={{color:'orange', height:'200px', backgroundColor:'#333'}}> Hey About </div>
          )
     }
    
